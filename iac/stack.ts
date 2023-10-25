e4f9d9a6492ecb4e32a5106a3f0469af9b7de24d
fargito <fr.farge96@gmail.com>
2023-10-25 16:31:46 +0200

    feat: add api gateway

--- iac/stack.ts
+++ iac/stack.ts
@@ -1,3 +1,5 @@
+import { HttpApi, HttpMethod } from '@aws-cdk/aws-apigatewayv2-alpha';
+import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Architecture } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
@@ -7,15 +9,26 @@ export class TodoAppStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const httpApi = new HttpApi(this, 'HttpApi');
+
     // Cargo workspace directory
     Settings.WORKSPACE_DIR = 'rust_lambdas';
     Settings.TARGET = 'aarch64-unknown-linux-gnu';
 
-    new RustFunction(this, 'CreateTodo', {
+    const createTodoLambda = new RustFunction(this, 'CreateTodo', {
       package: 'create_todo',
       setupLogging: true,
       architecture: Architecture.ARM_64,
       environment: {},
+    });
+
+    httpApi.addRoutes({
+      path: '/todo',
+      methods: [HttpMethod.POST],
+      integration: new HttpLambdaIntegration(
+        'CreateTodoIntegration',
+        createTodoLambda,
+      ),
     });
   }
 }
