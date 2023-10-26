4278baf7ad5f8986ecbc405d5c75c1441ddd13ae
fargito <fr.farge96@gmail.com>
2023-10-26 11:01:33 +0200

    feat: put fake todo in dynamodb

--- rust_lambdas/create-todo/src/main.rs
+++ rust_lambdas/create-todo/src/main.rs
@@ -1,3 +1,6 @@
+use std::env;
+
+use aws_sdk_dynamodb::types::AttributeValue;
 use serde::Deserialize;
 use shared::*;
 
@@ -19,6 +22,21 @@ async fn main() -> Result<(), Error> {
 
 pub(crate) async fn handler(event: LambdaEvent<Request>) -> Result<String, Error> {
     info!("Request: {:?}", event);
+
+    let todos_table_name = env::var("TODOS_TABLE_NAME").expect("Missing TODOS_TABLE_NAME env var");
+
+    // initialize dynamodb client
+    let config = aws_config::load_from_env().await;
+    let dynamodb_client = aws_sdk_dynamodb::Client::new(&config);
+
+    dynamodb_client
+        .put_item()
+        .table_name(todos_table_name)
+        .item("PK", AttributeValue::S("TODO".into()))
+        .item("SK", AttributeValue::S("TEST".into()))
+        .item("content", AttributeValue::S("Miam".into()))
+        .send()
+        .await?;
 
     Ok("Hello world!".into())
 }
