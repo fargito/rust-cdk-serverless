3501acadc5066313bb18d5e8118902b78e1d86a0
fargito <fr.farge96@gmail.com>
2023-11-18 17:47:48 +0100

    chore: remove lambda_runtime dependency

--- rust_lambdas/create-todo/src/main.rs
+++ rust_lambdas/create-todo/src/main.rs
@@ -3,8 +3,7 @@ mod handler;
 use std::env;
 use std::time::Instant;
 
-use lambda_http::{service_fn, Error};
-use lambda_runtime::tower::ServiceExt;
+use lambda_http::{service_fn, tower::ServiceExt, Error};
 
 use shared::setup_logging;
 
@@ -28,7 +27,7 @@ async fn main() -> Result<(), Error> {
     let func = service_fn(|request| handler(request, &dynamodb_client, &todos_table_name))
         .map_result::<_, _, Error>(|res| match res {
             Ok(res) => Ok(res),
-            Err(err) => Ok((err.status_code, serde_json::Value::String(err.body))),
+            Err(err) => Ok((err.status_code, err.body.into())),
         });
     lambda_http::run(func).await?;
 
