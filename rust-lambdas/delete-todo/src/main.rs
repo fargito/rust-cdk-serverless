862998a0a15a6444280e7e51616b69cccd75617d
fargito <fr.farge96@gmail.com>
2024-04-26 14:16:23 +0200

    feat(lambdas): send on todo deleted event from delete todo

--- rust-lambdas/delete-todo/src/main.rs
+++ rust-lambdas/delete-todo/src/main.rs
@@ -21,16 +21,26 @@ async fn main() -> Result<(), Error> {
 
     let config = aws_config::load_defaults(BehaviorVersion::latest()).await;
     let dynamodb_client = aws_sdk_dynamodb::Client::new(&config);
+    let eventbridge_client = aws_sdk_eventbridge::Client::new(&config);
 
     let todos_table_name = env::var("TODOS_TABLE_NAME").expect("Missing TODOS_TABLE_NAME env var");
+    let event_bus_name = env::var("EVENT_BUS_NAME").expect("Missing EVENT_BUS_NAME env var");
 
     debug!("DynamoDB client initialized in {:.2?}", start.elapsed());
 
-    let func = service_fn(|request| handler(request, &dynamodb_client, &todos_table_name))
-        .map_result::<_, _, Error>(|res| match res {
-            Ok(res) => Ok(res),
-            Err(err) => Ok((err.status_code, err.body.into())),
-        });
+    let func = service_fn(|request| {
+        handler(
+            request,
+            &dynamodb_client,
+            &eventbridge_client,
+            &todos_table_name,
+            &event_bus_name,
+        )
+    })
+    .map_result::<_, _, Error>(|res| match res {
+        Ok(res) => Ok(res),
+        Err(err) => Ok((err.status_code, err.body.into())),
+    });
     lambda_http::run(func).await?;
 
     Ok(())
