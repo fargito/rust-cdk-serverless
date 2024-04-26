3274c8bcd098a50eeafa8491aba3b620a27e41a5
fargito <fr.farge96@gmail.com>
2024-04-26 14:28:51 +0200

    feat(lambdas): add error logs when unable to send message to eventbridge

--- rust-lambdas/on-todo-deleted/src/handler.rs
+++ rust-lambdas/on-todo-deleted/src/handler.rs
@@ -1,21 +1,13 @@
 use aws_lambda_events::eventbridge::EventBridgeEvent;
 use aws_sdk_dynamodb::types::AttributeValue;
-use lambda_runtime::{
-    tracing::{error, info},
-    Error, LambdaEvent,
-};
+use lambda_runtime::{tracing::error, Error, LambdaEvent};
 use shared::Todo;
 
 pub(crate) async fn handler(
-    event: LambdaEvent<EventBridgeEvent<Todo>>,
+    _event: LambdaEvent<EventBridgeEvent<Todo>>,
     dynamodb_client: &aws_sdk_dynamodb::Client,
     todos_table_name: &str,
 ) -> Result<(), Error> {
-    info!(
-        "Hello from async lambda, we have received message: {:?}",
-        event.payload.detail.description
-    );
-
     dynamodb_client
         .update_item()
         .table_name(todos_table_name)
