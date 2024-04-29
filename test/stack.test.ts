6d89b82ff4554c0c68762f51981ef1c7916d5010
fargito <fr.farge96@gmail.com>
2024-04-29 22:21:08 +0200

    test(iac): simplify architectures test

--- test/stack.test.ts
+++ test/stack.test.ts
@@ -1,5 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
-import { Capture, Template } from 'aws-cdk-lib/assertions';
+import { Match, Template } from 'aws-cdk-lib/assertions';
 
 import * as TodoApp from '../iac/stack';
 
@@ -9,37 +9,14 @@ test('Resources are properly created', () => {
 
   const template = Template.fromStack(stack);
 
-  const runtimeCapture = new Capture();
-  const architectureCapture = new Capture();
-
-  template.hasResourceProperties('AWS::Lambda::Function', {
-    Architectures: architectureCapture,
-    Runtime: runtimeCapture,
-  });
-
-  let architectureCaptureCount = 0;
-  while (architectureCapture.next()) {
-    architectureCaptureCount++;
-
+  template.allResourcesProperties('AWS::Lambda::Function', {
     // all functions should have arm64 architecture
-    expect(architectureCapture.asArray()).toEqual(['arm64']);
-  }
-
-  // we should have at least one lambda resource
-  expect(architectureCaptureCount).toBeGreaterThan(0);
-
-  let runtimeCaptureCount = 0;
-  while (runtimeCapture.next()) {
-    runtimeCaptureCount++;
-
-    // all functions should have either nodejs or provided runtime
-    expect(['provided.al2023', 'nodejs20.x']).toContain(
-      runtimeCapture.asString(),
-    );
-  }
-
-  // we should have at least one lambda resource
-  expect(runtimeCaptureCount).toBeGreaterThan(0);
+    Architectures: Match.arrayEquals(['arm64']),
+    // all functions should have json logging
+    // LoggingConfig: { LogFormat: 'JSON' },
+    // all functions should have the same runtime
+    // Runtime: 'provided.al2023',
+  });
 
   template.resourceCountIs('AWS::Events::EventBus', 1);
   template.resourceCountIs('AWS::DynamoDB::Table', 2);
