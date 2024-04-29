97b850ffd483f6166dec21fdf14a348c9eddda2a
fargito <fr.farge96@gmail.com>
2024-04-29 18:35:49 +0200

    test(iac): assert runtime and architecture

--- test/stack.test.ts
+++ test/stack.test.ts
@@ -1,5 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
-import { Template } from 'aws-cdk-lib/assertions';
+import { Capture, Template } from 'aws-cdk-lib/assertions';
 
 import * as TodoApp from '../iac/stack';
 
@@ -9,9 +9,37 @@ test('Resources are properly created', () => {
 
   const template = Template.fromStack(stack);
 
+  const runtimeCapture = new Capture();
+  const architectureCapture = new Capture();
+
   template.hasResourceProperties('AWS::Lambda::Function', {
-    Architectures: ['arm64'],
+    Architectures: architectureCapture,
+    Runtime: runtimeCapture,
   });
+
+  let architectureCaptureCount = 0;
+  while (architectureCapture.next()) {
+    architectureCaptureCount++;
+
+    // all functions should have arm64 architecture
+    expect(architectureCapture.asArray()).toEqual(['arm64']);
+  }
+
+  // we should have at least one lambda resource
+  expect(architectureCaptureCount).toBeGreaterThan(0);
+
+  let runtimeCaptureCount = 0;
+  while (runtimeCapture.next()) {
+    runtimeCaptureCount++;
+
+    // all functions should have either nodejs or provided runtime
+    expect(['provided.al2023', 'nodejs20.x']).toContain(
+      runtimeCapture.asString(),
+    );
+  }
+
+  // we should have at least one lambda resource
+  expect(runtimeCaptureCount).toBeGreaterThan(0);
 
   template.resourceCountIs('AWS::Events::EventBus', 1);
   template.resourceCountIs('AWS::DynamoDB::Table', 2);
